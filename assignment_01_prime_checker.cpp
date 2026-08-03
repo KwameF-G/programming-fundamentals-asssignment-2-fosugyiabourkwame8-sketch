6772e4d40d6adbd8538dabeb753d148de429ff60
Kwame Fosu-Gyiabour <kfosu-gyiabour@st.ug.edu.gh>
2026-08-02 21:38:51 -0700

    defined is_prime in C++ to code for prime numbers in the main block

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,29 @@
 #include <iostream>
 using namespace std;
 
+// Function to check if the number is prime
+bool isPrime(int n) {
+    if (n < 2) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        cout << number << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
